06f7b0a92080a04d27ba7a8a8a60e2ce2940fd24
GiorgyArmani <giorgymodoginseng@gmail.com>
2025-11-21 12:25:14 -0300

    tes

--- supabase/fix_fk_constraint.sql
+++
@@ -1,5 +0,0 @@
--- Remove Foreign Key Constraint on profiles.id
--- This is necessary because we are using Wallet Auth, so profiles won't always match auth.users
-
-ALTER TABLE public.profiles
-DROP CONSTRAINT IF EXISTS profiles_id_fkey;
